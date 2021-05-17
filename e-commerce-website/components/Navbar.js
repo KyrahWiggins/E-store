18c23e500fdb6a12f50f6f6e7afac59eee40767a
KyrahWiggins <kyrah.wiggins@gmail.com>
2021-05-17 17:16:32 -0400

    Update 5/17
    pages are linked

--- e-commerce-website/components/Navbar.js
+++ e-commerce-website/components/Navbar.js
@@ -1,9 +1,43 @@
+import styled from 'styled-components';
+import Link from 'next/link';
+
+const Nav = styled.nav`
+  height: 80px;
+  background: #000;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  color: #fff;
+`;
+
+const StyledLink = styled.a`
+  padding: 0rem 2rem;
+`;
+
 const Navbar = () => {
-    return (
-        <div>
-            <h1>NavBar</h1>
-        </div>
-    );
+  return (
+    <Nav>
+      <div>
+        <Link href='/' passHref>
+          <StyledLink>KYREAUX KLOTHING</StyledLink>
+        </Link>
+      </div>
+      <div>
+        <Link href='/' passHref>
+          <StyledLink>HOME</StyledLink>
+        </Link>
+        <Link href='/about' passHref>
+          <StyledLink>ABOUT</StyledLink>
+        </Link>
+        <Link href='/contact' passHref>
+          <StyledLink>CONTACT</StyledLink>
+          </Link>
+          <Link href='/product' passHref>
+          <StyledLink>PRODUCT</StyledLink>
+          </Link>
+      </div>
+    </Nav>
+  );
 };
 
 export default Navbar;
--- e-commerce-website/pages/product.js
+++ e-commerce-website/pages/product.js
@@ -2,14 +2,15 @@ import Head from 'next/head'
 import Navbar from '../components/Navbar'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Contact() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Ky's E-store</title>
+        <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Navbar />
+      <h1>Product</h1>
     </div>
   );
 }
